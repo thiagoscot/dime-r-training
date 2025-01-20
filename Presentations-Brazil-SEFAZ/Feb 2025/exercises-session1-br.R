c696251564fb21f2546a6044739a5dbd866f4e19
Thiago Scot <31219574+thiagoscot@users.noreply.github.com>
2025-01-19 23:02:05 -0500

    Updates

--- Presentations-Brazil-SEFAZ/Feb 2025/exercises-session1-br.R
+++ Presentations-Brazil-SEFAZ/Feb 2025/exercises-session1-br.R
@@ -5,11 +5,11 @@ x3 <- x1 + x2
 print(x3)
 
 # Exercise 4
-df_pelotas <- subset(small_business_2019_br,
-                   region == "Pelotas")
-View(df_pelotas)
-
-# Exercise 5
+base_outubro <- subset(base_nfe,
+                   mes_emissao == 10)
+View(base_outubro)
+  
+# Exercise 5 (Vetores)
 v1 <- c(3, 8, 10)
 v2 <- c(7, 2, 5)
 result1 <- v1 + v2
@@ -18,5 +18,5 @@ print(result1)
 print(result2)
 
 # Exercise 6
-summary_table <- summary(small_business_2019_br)
-print(summary_table)
+summary_table <- summary(base_nfe)
+print(base_nfe)
